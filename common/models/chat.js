8cc2cc009500aaeb3898603662f0d7a23c5123ea
chicloon <chicloon@gmail.com>
2018-06-29 13:13:52 +0900

    working on models

--- common/models/chat.js
+++ common/models/chat.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(Chat) {
+  // List all chat members and their messages
+  Chat.listMessages = function(cb) {
+    Chat.find({include: 'messages'}, cb);
+  };
 
+  Chat.remoteMethod('listMessages', {
+    returns: {arg: 'messages', type: 'array'},
+    http: {path: '/list-messages', verb: 'get'},
+  });
+
+  Chat.membersMessages = function(id, cb) {
+    Chat.findById(id, {include: {messages: 'author'}}, cb);
+  };
+
+  Chat.remoteMethod('membersMessages', {
+    accepts: {arg: 'id', type: 'number'},
+    returns: {arg: 'Messages', type: 'array'},
+
+    http: {path: '/membersMessages', verb: 'get'},
+  });
 };
