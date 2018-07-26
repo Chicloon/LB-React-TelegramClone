66612923b7fd3c08d8c4fe4a71b73e749d78ee68
chicloon <chicloon@gmail.com>
2018-07-26 15:38:22 +0900

    working with models

--- common/models/chat.js
+++ common/models/chat.js
@@ -2,23 +2,29 @@
 
 module.exports = function(Chat) {
   // List all chat members and their messages
-  Chat.listMessages = function(cb) {
-    Chat.find({include: 'messages'}, cb);
+  Chat.listMessages = function(id, cb) {
+    Chat.findById(id, {include: {chatMessages: ['author']}}, cb);
   };
 
   Chat.remoteMethod('listMessages', {
+    accepts: {arg: 'id', type: 'number'},
     returns: {arg: 'messages', type: 'array'},
     http: {path: '/list-messages', verb: 'get'},
   });
 
-  Chat.membersMessages = function(id, cb) {
-    Chat.findById(id, {include: {messages: 'author'}}, cb);
-  };
+  // Chat.addMember = function(chatId, personId, cb) {
+  //   var chat = Chat.findById(chatId);
 
-  Chat.remoteMethod('membersMessages', {
-    accepts: {arg: 'id', type: 'number'},
-    returns: {arg: 'Messages', type: 'array'},
+  // };
 
-    http: {path: '/membersMessages', verb: 'get'},
-  });
+  // Chat.membersMessages = function(id, cb) {
+  //   Chat.findById(id, {include: {messages: 'author'}}, cb);
+  // };
+
+  // Chat.remoteMethod('membersMessages', {
+  //   accepts: {arg: 'id', type: 'number'},
+  //   returns: {arg: 'Messages', type: 'array'},
+
+  //   http: {path: '/membersMessages', verb: 'get'},
+  // });
 };
