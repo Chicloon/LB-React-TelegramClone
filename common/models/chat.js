35225d62faf4e96985f2a69d113821a0ebda2ed8
chicloon <chicloon@gmail.com>
2018-07-27 09:55:50 +0900

    need to fix chat-person relations

--- common/models/chat.js
+++ common/models/chat.js
@@ -12,6 +12,20 @@ module.exports = function(Chat) {
     http: {path: '/list-messages', verb: 'get'},
   });
 
+  Chat.listMembers = function(id, cb) {
+    Chat.findById(id, {include: {chatMembers: ['member']}}, cb);
+
+    Chat.findById(id, {include: ['chatMessages', 'chatMembers']}).then(res => {
+      console.log('Chat by id ===', id, res);
+    });
+  };
+
+  Chat.remoteMethod('listMembers', {
+    accepts: {arg: 'id', type: 'number'},
+    returns: {arg: 'members', type: 'array'},
+    http: {path: '/list-members', verb: 'get'},
+  });
+
   // Chat.addMember = function(chatId, personId, cb) {
   //   var chat = Chat.findById(chatId);
 
